109e9004a1e60acae26bb1b2a94bb3bf3f288bbc
Woloda <dorozovecvoloda@gmail.com>
2020-11-21 12:17:46 +0200

    Lab__7_3_1

---
+++ Lab_7.3(1)/Lab_7.3(1).cpp
@@ -0,0 +1,89 @@
+﻿//(Ітераційний спосіб)(1 Завдання)
+#include <iostream>
+#include <Windows.h>
+#include <iomanip>
+
+void matriz_create(int** matriz, const int line, const int column, const int Low, const int High);
+void matriz_print(int** matriz, const int line, const int column);
+int matriz_sum(int** matriz, const int line, const int column);
+
+int main(void)
+{
+	srand((unsigned)time(NULL));
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	using std::cout;
+	using std::endl;
+
+	const int line = 3;
+	const int column = 3;
+
+	int** matriz = new int* [line];
+	for (int index = 0; index < line; index++)
+		matriz[index] = new int[column];
+
+	int High = 97;
+	int Low = 16;
+	matriz_create(matriz, line, column, Low, High);
+	cout << "Сформований масив: " << endl;
+	matriz_print(matriz, line, column);
+
+	cout << endl;
+	int suma = matriz_sum(matriz, line, column);
+	cout << "Сума елементів матриці за вказаним критерієм: " << suma << endl;
+	cout << endl;
+
+	delete[] matriz;
+
+	return 0;
+}
+
+void matriz_create(int** matriz, const int line, const int column, const int Low, const int High)
+{
+	using std::cout;
+	using std::endl;
+	using std::cin;
+	for (int index = 0; index < line; index++)
+	{
+		for (int index_j = 0; index_j < column; index_j++)
+		{
+			cout << "a[" << index << "][" << index_j << "] = ";
+			cin >> matriz[index][index_j];
+		}
+		cout << endl;
+	}
+}
+
+void matriz_print(int** matriz, const int line, const int column)
+{
+	using std::cout;
+	using std::endl;
+	cout << endl;
+	for (int index = 0; index < line; index++)
+	{
+		for (int index_j = 0; index_j < column; index_j++)
+			cout << std::setw(4) << matriz[index][index_j];
+		cout << endl;
+	}
+	cout << endl;
+}
+
+int matriz_sum(int** matriz, const int line, const int column)
+{
+	int sum = 0;
+	for (int index = 0; index < line; index++)
+	{
+		for (int index_jl = 0; index_jl < column; index_jl++)
+		{
+			if (matriz[index][index_jl] < 0)
+			{
+				for (int index_j = 0; index_j < column; index_j++)
+					sum += matriz[index][index_j];
+				index_jl = column;
+			}
+		}
+	}
+	return sum;
+}
---
+++ UnitTest(Lab_7.3(1))/UnitTest(Lab_7.3(1)).cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_7.3(1)/Lab_7.3(1).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab731
+{
+	TEST_CLASS(UnitTestLab731)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int const line = 3;
+			int const column = 3;
+			int** matriz = new int* [line];
+			for (int index = 0; index < line; index++)
+				matriz[index] = new int[column];
+			matriz[0][0] = -9; matriz[0][1] = 1;matriz[0][2] = 2;matriz[1][0] = 3;matriz[1][1] = 4;matriz[1][2] = 5;matriz[2][0] = 6;matriz[2][1] = 7;matriz[2][2] = 8;
+			int suma = matriz_sum(matriz, line, column);
+			Assert::AreEqual(suma, -6);
+			delete[] matriz;
+		}
+	};
+}
